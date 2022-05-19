e0ae9b06b4c2a1e5fb0135b1104dee6de9d38397
Matthew <martinms@rose-hulman.edu>
2022-05-19 00:48:19 -0400

    finished likes/views

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -51,6 +51,7 @@ rhit.currentUserTemp = class {
 		this.bookmarked = bookmarked;
 
 		if(window.location.href.includes("detailPostPage.html")){
+			currentPage.addView();
 			currentPage.updateView();
 		}
 
@@ -66,8 +67,9 @@ rhit.currentUserTemp = class {
 
 		document.querySelector("#recNav").style.display = "block";
 		document.querySelector("#bookNav").style.display = "block";
-		document.querySelector("#fab").style.display = "block";
-
+		if(document.querySelector("#fab")){
+			document.querySelector("#fab").style.display = "block";
+		}
 	}
 }
 
@@ -121,12 +123,16 @@ rhit.QueryMachine = class {
 	addView(postId) {
 		this.getPost(postId).then((post) => {
 			post.views++;
-			this.editPost(post);
-			if(rhit.currentUser){
-				this.userData.where("uid", "==", uid).get().then((docs) => {
+			this.posts.doc(postId).update({
+				"views": post.views
+			});
+			if(rhit.currentUser && !rhit.currentUser.viewed.includes(postId)){
+				this.userData.where("uid", "==", rhit.currentUser.uid).get().then((docs) => {
 					docs.forEach((doc) => {
+						var viewed = rhit.currentUser.viewed;
+						viewed.push(postId);
 						this.userData.doc(doc.id).update({
-							"viewed": doc.data().viewed.push(postId)
+							"viewed": viewed
 						});
 					})
 				});
@@ -135,14 +141,25 @@ rhit.QueryMachine = class {
 	}
 
 	addLike(postId) {
+
+		if(rhit.currentUser.liked.includes(postId)){
+			return;
+		}
+
 		this.getPost(postId).then((post) => {
 			post.likes++;
-			this.editPost(post);
+			this.posts.doc(postId).update({
+				"likes": post.likes
+			});
 			if(rhit.currentUser){
-				this.userData.where("uid", "==", uid).get().then((docs) => {
+				this.userData.where("uid", "==", rhit.currentUser.uid).get().then((docs) => {
 					docs.forEach((doc) => {
+
+						var liked = rhit.currentUser.liked;
+						liked.push(postId);
+
 						this.userData.doc(doc.id).update({
-							"liked": doc.data().liked.push(postId)
+							"liked": liked
 						});
 					})
 				});
@@ -270,19 +287,24 @@ function createFullCard(post) {
 	var html = 
 	`<div class="card">
 		<div class="card-body"> 
-			<h5 class="card-title">${post.title}</h5>
-			<p class="card-text"><small class="text-muted">${post.timestamp} / ${post.views} Views / ${post.likes} Likes
+			<h5 class="card-title display-inline-block">${post.title}</h5> 
+			<button id="likeButton" type="button" style="float:right;"  class="btn btn-default btn-sm .display-inline-block">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
+					<path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
+				</svg>
+			</button>
+			<p class="card-text"><small class="text-muted">${post.author} / ${post.timestamp} / ${post.views} Views / ${post.likes} Likes
 			</small></p>
 
 			<!-- put link to "link for tagged posts goes here" -->
 			<p class="card-text"><small class="text-muted">
 				${tagshtml}
 			</small></p>
-			
+		
 			<br>
 			<p class="card-text">${post.content}</p>
 		</div>
-	</div>`
+  	</div>`
 	;
 	html = html.trim();
 	template.innerHTML = html;
@@ -392,6 +414,10 @@ rhit.detailPage = class {
 		this.updateView();
 	}
 
+	addView() {
+		queryMachine.addView(this.postId);
+	}
+
 	updateView() {
 
 		queryMachine.getPost(this.postId).then((post) => {
@@ -403,6 +429,11 @@ rhit.detailPage = class {
 			if(post.author == rhit.currentUser.name){
 				document.querySelector("#dropdown").style.display = "block";
 			}
+
+			document.querySelector("#likeButton").addEventListener("click", (event) => {
+				queryMachine.addLike(this.postId);
+			})
+
 		});
 
 		queryMachine.getReplies(this.postId).then((replies) => {
@@ -434,21 +465,23 @@ rhit.addPostPage = class {
 		var tags = document.querySelector("#tagInput");
 		var content = document.querySelector("#contentInput");
 
-		queryMachine.getPost(this.postId).then((post) => {
-			if(post){
-				title.value = post.title;
-				content.value = post.content;
-				post.tags.forEach(tag => {
-					tags.value += `#${tag} `;
-				});
-				this.author = post.author;
-			}
-		});
+		if(this.postId){
+			queryMachine.getPost(this.postId).then((post) => {
+				if(post){
+					title.value = post.title;
+					content.value = post.content;
+					post.tags.forEach(tag => {
+						tags.value += `#${tag} `;
+					});
+					this.author = post.author;
+				}
+			});
+		}
 
 		document.querySelector("#submitButton").addEventListener("click", (event) => {
 			if(title.value.trim() && content.value.trim()){
 				var newPost = new rhit.post(this.postId, title.value, rhit.currentUser.name, content.value, 0, 0, 0, firebase.firestore.Timestamp.now(), this.markTags(tags.value));
-
+				
 				if(this.postId != null){
 					if(this.author == rhit.currentUser.name){
 						queryMachine.editPost(newPost);
@@ -492,8 +525,6 @@ rhit.addPostPage = class {
 				tags.splice(i, 1);
 			}
 		}
-
-		console.log(tags);
 
 		return tags;
 		
