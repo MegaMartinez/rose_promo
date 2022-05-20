6eab77be92cde1490ee94b0eaedf4a7a44262d99
SeokHyun Chang <89899621+rhit-changs@users.noreply.github.com>
2022-05-20 18:20:33 -0400

    minor format change

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -63,13 +63,11 @@ rhit.currentUserTemp = class {
 			currentPage.listBookMarks(this.bookmarked);
 		}
 
-		if(document.querySelector("#signInNavItem")){
-			document.querySelector("#signInNavItem").innerHTML = "Sign-Out";
-			document.querySelector("#signInNavItem").href = "/";
-			document.querySelector("#signInNavItem").addEventListener("click", (event) => {
-				authentication.signOut();
-			});
-		}
+		document.querySelector("#signInNavItem").innerHTML = "Sign-Out";
+		document.querySelector("#signInNavItem").href = "/";
+		document.querySelector("#signInNavItem").addEventListener("click", (event) => {
+			authentication.signOut();
+		});
 
 		document.querySelector("#recNav").style.display = "block";
 		document.querySelector("#bookNav").style.display = "block";
@@ -344,14 +342,14 @@ function createPreviewCard(post) {
 		<div class="card">
 			<div class="card-body">
 				<a href="/detailPostPage.html?id=${post.id}">
-					<h5 class="card-title display-inline-block">${post.title}</h5>
+					<h5 class="card-title">${post.title}</h5>
 				</a>
 				<button type="button" style="float:right; display: ${display};"  class="btn btn-default btn-sm .display-inline-block deleteBookmarkButton" data-postid="${post.id}">
 					<svg xmlns="http://www.w3.org/2000/svg"  width="16" height="16" fill="currentColor" class="bi bi-x-lg display-inline-block totheright" viewBox="0 0 16 16">
 						<path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
 					</svg>
 				</button>
-				<p class="card-text display-inline-block"><small class="text-muted">${post.author} / ${post.timestamp} / ${post.views} views / ${post.likes} likes
+				<p class="card-text"><small class="text-muted">${post.author} / ${post.timestamp} / ${post.views} views / ${post.likes} likes
 					</small></p>
 				<p class="card-text tag"><small class="text-muted">
 					${tagshtml}
